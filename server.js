f625525bc2b4846b567a201b937a27fb3024942b
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-28 17:09:06 +0530

    ci: :poop: skeleton code added, paths need to be updated : master

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = 
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port : " + app.get("port"));
+});
