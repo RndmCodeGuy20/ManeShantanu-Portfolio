17befc622a4286ea9ac656527a1d095d1b6b5b26
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-05-02 23:23:23 +0530

    feat: :sparkles: Form data retrieval added, submit button design changed : master

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/thankyou", function (req, res) {
+  res.sendFile(__dirname + "/thankyou.html");
+});
+
+app.post("/thankyou", function (req, res) {
+  console.log(req.body);
+  res.send("Thank you");
+});
+
+// console.log(__dirname + "/Countdown Timer/index.html");
+
+// app.get("/Countdown%20Timer/", function (req, res) {
+//   res.sendFile(__dirname + "/Countdown Timer/index.html");
+//   console.log(__dirname + "/Countdown Timer/index.html");
+// });
+
 app.set("port", process.env.PORT || 3000);
 
 app.listen(process.env.PORT || 3000, function () {
