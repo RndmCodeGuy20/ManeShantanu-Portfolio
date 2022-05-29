7d9e941d400ae362434358c2ec3886d41daac373
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-05-29 10:16:10 +0530

    style: :speech_balloon: minimal text changes : master

--- server.js
+++ server.js
@@ -84,5 +84,5 @@ app.get("/Quote", function (req, res) {
 app.set("port", process.env.PORT || 3000);
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on http://127.0.0.1:3000");
+  console.log("Server started on http://localhost:3000");
 });
